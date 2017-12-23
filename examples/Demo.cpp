b25f29ab2424b3b3d393266ae9c62e429eda0c16
ccrisrober <ccrisrober@gmail.com>
2017-12-23 16:42:02 +0100

    Threading rendering wip

--- examples/Demo.cpp
+++ examples/Demo.cpp
@@ -4,6 +4,140 @@ using namespace lava;
 
 #define VK_USE_PLATFORM_WIN32_KHR
 
+#include <vector>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+
+/*template<typename T, typename ...Args>
+std::unique_ptr<T> make_unique( Args&& ...args )
+{
+  return std::unique_ptr<T>( new T( std::forward<Args>( args )... ) );
+}*/
+
+namespace foo
+{
+  class Thread
+  {
+  private:
+    bool destroying = false;
+    std::thread worker;
+    std::queue<std::function<void( )>> jobQueue;
+    std::mutex queueMutex;
+    std::condition_variable condition;
+
+    // Loop through all remaining jobs
+    void queueLoop( )
+    {
+      while ( true )
+      {
+        std::function<void( )> job;
+        {
+          std::unique_lock<std::mutex> lock( queueMutex );
+          condition.wait( lock, [this] { return !jobQueue.empty( ) || destroying; } );
+          if ( destroying )
+          {
+            break;
+          }
+          job = jobQueue.front( );
+        }
+
+        job( );
+
+        {
+          std::lock_guard<std::mutex> lock( queueMutex );
+          jobQueue.pop( );
+          condition.notify_one( );
+        }
+      }
+    }
+
+  public:
+    Thread( )
+    {
+      worker = std::thread( &Thread::queueLoop, this );
+    }
+
+    ~Thread( )
+    {
+      if ( worker.joinable( ) )
+      {
+        wait( );
+        queueMutex.lock( );
+        destroying = true;
+        condition.notify_one( );
+        queueMutex.unlock( );
+        worker.join( );
+      }
+    }
+
+    // Add a new job to the thread's queue
+    void addJob( std::function<void( )> function )
+    {
+      std::lock_guard<std::mutex> lock( queueMutex );
+      jobQueue.push( std::move( function ) );
+      condition.notify_one( );
+    }
+
+    // Wait until all work items have been finished
+    void wait( )
+    {
+      std::unique_lock<std::mutex> lock( queueMutex );
+      condition.wait( lock, [this]( ) { return jobQueue.empty( ); } );
+    }
+  };
+
+  class ThreadPool
+  {
+  public:
+    std::vector<std::unique_ptr<Thread>> threads;
+
+    // Sets the number of threads to be allocted in this pool
+    void setThreadCount( uint32_t count )
+    {
+      threads.clear( );
+      for ( auto i = 0; i < count; i++ )
+      {
+        threads.push_back( make_unique<Thread>( ) );
+      }
+    }
+
+    // Wait until all threads have finished their work items
+    void wait( )
+    {
+      for ( auto &thread : threads )
+      {
+        thread->wait( );
+      }
+    }
+  };
+}
+
+struct ThreadPushConstantBlock
+{
+  glm::mat4 model;
+};
+
+struct ObjectData
+{
+  glm::mat4 model;
+};
+
+struct ThreadData
+{
+  std::shared_ptr< CommandPool > cmdPool;
+  std::vector< vk::CommandBuffer > cmdBuffers;
+  std::vector< ThreadPushConstantBlock > pushConstantBlock;
+  std::vector< ObjectData > objectData;
+};
+
+std::vector< ThreadData > threadData;
+
+foo::ThreadPool threadPool;
+uint32_t numThreads;
+
 class CustomRenderer : public VulkanWindowRenderer
 {
 public:
@@ -245,6 +379,13 @@ int main( void )
     extensions.size( ),
     extensions.data( )
   ) );
+
+
+  // Get number of max. concurrrent threads
+  numThreads = std::thread::hardware_concurrency( );
+  assert( numThreads > 0 );
+
+  threadPool.setThreadCount( numThreads );
 
   CustomVkWindow w;
   w.setVulkanInstance( instance );
